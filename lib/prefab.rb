c1c5f8791a4dceb4a715b365e067402fd09a7186
John Perkins <jeperkins4@gmail.com>
2017-02-27 14:57:45 -0500

    Fix to generator

--- lib/prefab.rb
+++ lib/prefab.rb
@@ -6,12 +6,12 @@ module Prefab
       config.app_generators.stylesheets false
 
       initializer 'prefab.setup', group: :all do |app|
-        app.config.less.paths << ::File.expand_path('../../vendor/twitter/bootstrap/less', __FILE__) if app.config.respond_to?(:less)
-        app.config.assets.paths << ::File.expand_path('../../vendor/twitter/bootstrap/sass', __FILE__) if app.config.respond_to?(:sass)
+        #app.config.less.paths << ::File.expand_path('../../vendor/twitter/bootstrap/less', __FILE__) if app.config.respond_to?(:less)
+        #app.config.assets.paths << ::File.expand_path('../../vendor/twitter/bootstrap/sass', __FILE__) if app.config.respond_to?(:sass)
 
-        app.config.assets.paths << ::Rails.root.join('app', 'assets', 'fonts')
+        #app.config.assets.paths << ::Rails.root.join('app', 'assets', 'fonts')
 
-        app.config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
+        #app.config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
       end
     end
   end
